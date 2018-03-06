f7af7855f8cf29614fb1deb1f5b0a0445306e209
Dennis Lin <dennis@denniss-macbook-pro.local>
2018-03-06 14:36:27 -0500

    Initial Commit

---
+++ Pedigree/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pedigree
+//
+//  Created by Dennis Lin on 3/6/18.
+//  Copyright © 2018 TungHo Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
